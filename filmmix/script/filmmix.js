d174005f74b21f24ddfcf5712b9385c73736b286
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-19 10:06:59 -0300

    Separate files

---
+++ filmmix/script/filmmix.js
@@ -0,0 +1,12 @@
+function redirecionar(arquivo) {
+    window.location.href = arquivo;
+}
+function pagina_nova(select) {
+    const selectedValue = select.value;
+    if (selectedValue) {
+        window.location.href = selectedValue;
+    }
+}
+function clicar_menu(){
+    
+}
