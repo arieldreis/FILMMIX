2fb74c424ecfcb634c97e9ec7267b22feba89d08
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-08-24 22:12:08 -0300

    Now, it work

--- filmmix/script/scriptBackEnd.js
+++ filmmix/script/scriptBackEnd.js
@@ -4,6 +4,13 @@ import mysql from 'mysql2/promise';
 const app = express();
 const PORT = 3050;
 
+// Middleware para ler JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Direcionamento para outra pagina
+app.use(express.static('public'));
+
 app.post('/createAccount', async (req, res) => {
     const mydb = mysql.createPool({
         host: "localhost",
@@ -22,6 +29,7 @@ app.post('/createAccount', async (req, res) => {
             'INSERT INTO dadosCliente(usuario, senha, cadPessoaFisico, email, nascimento, estado) VALUES(?, ?, ?, ?, ?, ?)',
             [usuario, senha, cpf, email, nascimento, estado]
         );
+        res.redirect('/sucesso.html');
     }catch(error){
         res.status(500).json({
             error: "Erro no banco de dados."
